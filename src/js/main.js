e72f6deee23702a7bf72fe1b55f9d34450c463ba
James Delibas <jdelibas@gmail.com>
2014-04-12 19:26:27 +0100

    Start Game
    Instantiate the canvas class.Removed jquery dependency, wasnt needed
    since manipulated dom is loaded from javascript.

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,3 @@
-// Create the space object in the global scope
-// All other game related objects should be a
-// child of this object.
-window.space = {};
+var spaceGame = {};
 
-$(window).load(function() {
-game.init();
-});
-
-var game = {
-  // Start initializing objects, preloading assets and display start screen
-  init: function(){
-	// Hide all game layers and display the start screen
-	$('.gamelayer').hide();
-	$('#gamestartscreen').show();
-	
-	//Get handler for game canvas and context
-	game.canvas = $('#gamecanvas')[0];
-	game.context = game.canvas.getContext('2d');
-  },
-}
+spaceGame.canvas = new ENGINE.Canvas();
