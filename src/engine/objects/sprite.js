5a33f5a0af466e3d18e05d19172d11c71ade2f15
James Delibas <jdelibas@gmail.com>
2014-04-13 19:01:12 +0100

    Added offset to sprite
    Offsets position to the centre

--- src/engine/objects/sprite.js
+++ src/engine/objects/sprite.js
@@ -4,8 +4,11 @@ ENGINE.Sprite = function(canvas, textureURL) {
 	this.dataType = "sprite";
 
 	this.canvas = canvas;
+	
     this.x = 0;
     this.y = 0;
+    this.offsetX = 0;
+    this.offsetY = 0;
 
     this.texture = new Image();
     this.texture.src = textureURL;
@@ -22,7 +25,8 @@ ENGINE.Sprite.prototype.setupTexture = function(){
 	this.texture.onload = function(){
 		this.width = this.texture.width;
 		this.height = this.texture.height;
-
+		this.offsetX = this.width / 2;
+		this.offsetY = this.height / 2;
 		this.canvas.drawList.push(this);
 	}.bind(this);
 
@@ -30,7 +34,7 @@ ENGINE.Sprite.prototype.setupTexture = function(){
 
 ENGINE.Sprite.prototype.setPosition = function(x,y){
 
-	this.x = x;
-	this.y = y;
+	this.x = x - this.offsetX;
+	this.y = y - this.offsetY;
 
 };
