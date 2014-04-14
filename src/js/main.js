3ff28b2861d00dc7337d39c67b5a6fe53ace39fd
James Delibas <jdelibas@gmail.com>
2014-04-14 20:39:54 +0100

    Using the new functions and modified sprites

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,30 @@ var spaceGame = {};
 
 spaceGame.canvas = new ENGINE.Canvas();
 
-spaceGame.sprite = new ENGINE.Sprite(spaceGame.canvas, 'image/sprites/ship.png');
+spaceGame.ship = new ENGINE.Sprite(spaceGame.canvas, 'image/sprites/ship.png');
 
-spaceGame.mouse = new ENGINE.Mouse(spaceGame.canvas.element, function(mouse){
-	spaceGame.sprite.setPosition(mouse.x,mouse.y);
-	spaceGame.sprite.rotate();
+spaceGame.mouse = new ENGINE.Mouse(spaceGame.canvas.element);
+
+
+spaceGame.mouse.mouseMove(function(mouse){
+
+	spaceGame.ship.setPosition(mouse.x - spaceGame.ship.offsetX, mouse.y - spaceGame.ship.offsetY);
+	spaceGame.ship.rotate();
+
 });
+
+spaceGame.mouse.mouseClick(function(){
+
+	var bullet = new ENGINE.Sprite(spaceGame.canvas, 'image/sprites/bullet.png');
+	bullet.setPosition(spaceGame.ship.x, spaceGame.ship.y);
+	bullet.move(0, -10);
+
+});
+
+spaceGame.mouse.mouseDown(function(){
+
+	var bullet = new ENGINE.Sprite(spaceGame.canvas, 'image/sprites/bullet.png');
+	bullet.setPosition(spaceGame.ship.x, spaceGame.ship.y);
+	bullet.move(0, -10);
+
+}, 200, "left");
