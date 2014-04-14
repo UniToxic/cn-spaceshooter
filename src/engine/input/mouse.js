573d1b7616ece97623a2ff91be626faadacdc819
James Delibas <jdelibas@gmail.com>
2014-04-14 20:36:08 +0100

    Mouse major update
    Added mouseClick, mousedown, mouseup and context.
    
    moucseclick now accepts a local callback so do all the other functions.
    
    context disables the right click context menu based on
    this.allowContext being true or false
    
    mousedown has an interval which can be set and the interval is cleared
    on mouse up
    
    mousedown also accepts and extra parameter to define which button
    (left,middle,right or wacom?)

--- src/engine/input/mouse.js
+++ src/engine/input/mouse.js
@@ -1,14 +1,15 @@
-ENGINE.Mouse = function(element,callback) {
+ENGINE.Mouse = function(element) {
 
 
 	this.element = element || window;
-	this.callback = callback;
 	this.getBounds();
 
     this.x = 0;
     this.y = 0;
 
-    this.mouseMove();
+    this.allowContext = false;
+    this.context();
+
     return this;
     
 };
@@ -29,13 +30,73 @@ ENGINE.Mouse.prototype.getMousePosition = function(event){
 
 };
 
-ENGINE.Mouse.prototype.mouseMove = function(){
+ENGINE.Mouse.prototype.mouseMove = function(callback){
 
 	this.element.addEventListener('mousemove', function(event) {
 
 		this.getMousePosition(event);
-		this.callback({x: this.x, y: this.y});
+		callback({x: this.x, y: this.y});
 
 	}.bind(this), false);
+
+};
+
+ENGINE.Mouse.prototype.mouseClick = function(callback){
+
+	this.element.addEventListener('click', function(event) {
+
+		callback();
+
+	}.bind(this), false);
+
+};
+
+ENGINE.Mouse.prototype.mouseDown = function(callback,interval,click){
+
+	this.element.addEventListener('mousedown', function(event) {
+
+    switch (event.which) {
+        case 1:
+        	if(click === "left")
+            this.mouseDownTimer = setInterval(callback, interval);
+            break;
+        case 2:
+        	if(click === "middle")
+            this.mouseDownTimer = setInterval(callback, interval);
+            break;
+        case 3:
+        	if(click === "right")
+            this.mouseDownTimer = setInterval(callback, interval);
+            break;
+        default:
+            alert('Are you using a wacom?!');
+    }
+
+
+		
+
+	}.bind(this), false);
+
+	this.mouseUp();
+};
+
+ENGINE.Mouse.prototype.mouseUp = function(callback){
+
+	this.element.addEventListener('mouseup', function(event) {
+
+		clearInterval(this.mouseDownTimer);
+
+		if(callback)
+			callback();
+
+	}.bind(this), false);
+
+};
+
+ENGINE.Mouse.prototype.context = function(){
+
+	this.element.oncontextmenu = function() {
+	     return this.allowContext;  
+	}.bind(this);
 
 };
